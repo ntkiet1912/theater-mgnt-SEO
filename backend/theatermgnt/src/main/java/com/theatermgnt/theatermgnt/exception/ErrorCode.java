3a2e552d47ec8f3b3cb6b9978fe29811f29c50f8
キエット <tuankietwork22@gmail.com>
2025-11-02 08:28:58 +0700

    fix-ErrorCode-eleminate-redundant-enum (#12)

--- backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/exception/ErrorCode.java
+++ backend/theatermgnt/src/main/java/com/theatermgnt/theatermgnt/exception/ErrorCode.java
@@ -5,15 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategotized error", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_TYPING(1001, "WRONG IN YOUR CODE", HttpStatus.BAD_REQUEST),
-    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
-    UNAUTHENTICATED(1006, "User not authenticated", HttpStatus.UNAUTHORIZED),
-    UNAUTHORIZE(1007, "You do not have permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
+
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR), // Code: 500
+    INVALID_KEY(1001, "Invalid message key", HttpStatus.BAD_REQUEST), // 404
+    UNAUTHORIZED(1002, "You do not have permissions", HttpStatus.FORBIDDEN), // 403
+    USER_EXISTED(1003, "User existed", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1004, "User not existed", HttpStatus.NOT_FOUND),
+    PHONE_NUMBER_EXISTED(1005, "Phone number has existed", HttpStatus.BAD_REQUEST),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED), // 401
+    ROLE_NOT_FOUND(1007, "Role not found", HttpStatus.NOT_FOUND), // 401
+    INVALID_TYPING(1008, "WRONG IN YOUR CODE", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1009, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1010, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    UNAUTHORIZE(1011, "You do not have permission", HttpStatus.FORBIDDEN),
+    INVALID_DOB(1012, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     //----
     CINEMA_EXISTED(2001, "Cinema existed", HttpStatus.BAD_REQUEST),
     CINEMA_NOT_EXISTED(2002, "Cinema not existed", HttpStatus.BAD_REQUEST),
@@ -24,7 +29,7 @@ public enum ErrorCode {
     PRICECONFIG_NOT_EXISTED(2007, "Price config not existed", HttpStatus.BAD_REQUEST),
     PRICECONFIG_EXISTED(2008, "Price config existed", HttpStatus.BAD_REQUEST),
     SEAT_NOT_EXISTED(2009, "Seat not existed", HttpStatus.BAD_REQUEST),
-    SEAT_EXISTED(20010, "Seat existed", HttpStatus.BAD_REQUEST),
+    SEAT_EXISTED(2010, "Seat existed", HttpStatus.BAD_REQUEST),
     ;
     private int code;
     private String message;
@@ -36,33 +41,4 @@ public enum ErrorCode {
         this.statusCode = statusCode;
     }
 
-}
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-
-import lombok.Getter;
-
-@Getter
-public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR), // Code: 500
-    INVALID_KEY(1001, "Invalid message key", HttpStatus.BAD_REQUEST), // 404
-    UNAUTHORIZED(1002, "You do not have permissions", HttpStatus.FORBIDDEN), // 403
-    USER_EXISTED(1003, "User existed", HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTED(1004, "User not existed", HttpStatus.NOT_FOUND),
-    PHONE_NUMBER_EXISTED(1005, "Phone number has existed", HttpStatus.BAD_REQUEST),
-    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED), // 401
-    ROLE_NOT_FOUND(1007, "Role not found", HttpStatus.NOT_FOUND), // 401
-
-    ;
-
-    ErrorCode(int code, String message, HttpStatusCode statusCode) {
-        this.code = code;
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-
-    private int code;
-    private String message;
-    private HttpStatusCode statusCode;
 }
